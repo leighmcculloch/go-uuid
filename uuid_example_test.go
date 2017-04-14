3907c9d602862214d619577cfcf15bfc11b54e3e
Leigh McCulloch <leigh@mcchouse.com>
2017-04-14 06:41:27 +0000

    Examples

--- uuid_example_test.go
+++ uuid_example_test.go
@@ -1,10 +1,18 @@
 package uuid_test
 
 import (
-	"github.com/leighmcculloch/go-uuid/timeuuid"
+	"fmt"
+
+	"github.com/leighmcculloch/go-uuid"
 )
 
-func Example_timeUUIDNow() {
-	_ = timeuuid.Now()
-	// Returns a UUID. e.g. 08827178-0ad4-11e7-b5df-b3f54921aa61
+func Example() {
+	id, _ := uuid.Parse("08827178-0ad4-11e7-b5df-b3f54921aa61")
+
+	fmt.Println("String:", id.String())
+	fmt.Println("Timestamp:", id.Timestamp())
+
+	// Output:
+	// String: 08827178-0ad4-11e7-b5df-b3f54921aa61
+	// Timestamp: 1290011724057047416
 }
